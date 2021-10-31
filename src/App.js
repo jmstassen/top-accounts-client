3b53ecbe41f35caf6b993eda7948a7f89f684912
John M. Stassen <john.stassen@gmail.com>
2021-10-31 15:35:57 -0700

    build out skeletal structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { fetchAccounts } from "./actions/accountActions";
 import AccountsList from './components/AccountsList';
 import AccountInput from './components/AccountInput';
+import SummaryContainer from './containers/SummaryContainer';
 
 class App extends Component {
   componentDidMount() {
@@ -13,10 +14,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <AccountInput />
-        <h1>Top Accounts</h1>
-        <AccountsList accounts={this.props.accounts} />
+      <div className="app">
+        <div className="container-card">
+            <SummaryContainer account={this.props.accounts.item} />
+        </div>
+        <div className="container-card">
+          <h1>Objectives Container</h1>
+        </div>
+        <div className="right-column-card">
+          <h1>My Top Accounts</h1>
+          <AccountsList accounts={this.props.accounts} />
+          <AccountInput />
+        </div>
+        <div className="activity-container">
+          <h1>Activities Container</h1>
+        </div>
       </div>
     );
   }
