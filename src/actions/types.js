8337ac5f957fd49bd3d809a43b08d216233fb703
John M. Stassen <john.stassen@gmail.com>
2021-11-05 13:39:16 -0700

    adding action types commented out

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,12 @@
 export const FETCH_ACCOUNTS = 'FETCH_ACCOUNTS';
 export const NEW_ACCOUNT = 'NEW_ACCOUNT';
+
+// export const CHANGE_CURRENT_ACCOUNT = 'CHANGE_CURRENT_ACCOUNT';
+
+// export const EDIT_ACCOUNT_NAME = 'EDIT_ACCOUNT_NAME';
+
+// export const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
+
+// export const ADD_NEW_ACTIVITY = 'ADD_NEW_ACTIVITY'
+// export const EDIT_ACTIVITY = 'EDIT_ACTIVITY';
+// export const DELETE_ACTIVITY = 'DELETE_ACTIVITY';
