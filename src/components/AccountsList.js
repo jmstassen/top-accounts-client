daa442714903ab429bc70ce2263a0a616b90e78a
John M. Stassen <john.stassen@gmail.com>
2021-11-07 13:15:19 -0800

    remake

--- src/components/AccountsList.js
+++ src/components/AccountsList.js
@@ -1,46 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchAccounts } from '../actions/accountActions'
 
+const AccountsList = (props) => {
 
-
-class AccountsList extends Component {
-  componentDidMount() {
-    this.props.fetchAccounts();
-  }
-
-  componentDidUpdate(nextProps) {
-    if (nextProps.new_account) {
-      this.props.accounts.unshift(nextProps.new_account)
-    }
-  }
-
-  accountsList = () => {
-    return this.props.accounts.map(account =>
-      <li key={account.id}>{account.account_name}</li>
-    )
-  }
-
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        {this.accountsList()}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {props.accounts.map(account => <li key={account.id}>{account.account_name}</li>)}
+    </div>
+  )
+  
 }
 
-AccountsList.propTypes = {
-  fetchAccounts: PropTypes.func.isRequired,
-  accounts: PropTypes.array.isRequired,
-  new_account: PropTypes.object
-}
-
-const mapStateToProps = state => ({
-   accounts: state.accounts.accounts,
-   new_account: state.accounts.new_account
-})
-
-export default connect(mapStateToProps, { fetchAccounts })(AccountsList)
+export default AccountsList
