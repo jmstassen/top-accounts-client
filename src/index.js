f95056e3735ee2f18f6ed82ec2faf87376b8edeb
John M. Stassen <john.stassen@gmail.com>
2021-11-07 09:39:46 -0800

    set up index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
-import store from "./store.js";
+// import { Provider } from "react-redux";
+// import store from "./store.js";
 
 ReactDOM.render(
   <React.StrictMode>
