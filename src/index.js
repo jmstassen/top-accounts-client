4a0f6400cd1077256ab699aa53695f893d8a4b61
John M. Stassen <john.stassen@gmail.com>
2021-11-07 09:52:42 -0800

    set up store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-
-import './index.css';
 import App from './App';
-// import { Provider } from "react-redux";
-// import store from "./store.js";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
