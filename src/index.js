66e524dff7889ec238144adfff08e06a72a3ecd3
John M. Stassen <john.stassen@gmail.com>
2021-11-07 21:57:44 -0800

    add Router

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -17,7 +19,9 @@ let store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>,
   document.getElementById('root')
 );
