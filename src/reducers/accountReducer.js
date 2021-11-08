97be3ce636ebdbe1c010e0eb28decd0e62ccae51
John M. Stassen <john.stassen@gmail.com>
2021-11-07 16:41:32 -0800

    add case for ADD_ACCOUNT

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_ACCOUNTS, NEW_ACCOUNT } from '../actions/types';
+import { FETCH_ACCOUNTS, ADD_ACCOUNT } from '../actions/types';
 
 export default function accountReducer(state = {accounts: [], current_account:{}}, action) {
   switch(action.type) {
     case FETCH_ACCOUNTS:
       return {accounts: action.payload, current_account: action.payload[0]}
+    case ADD_ACCOUNT:
+      return {...state, accounts: [...state.accounts, action.payload]}
     default:
       return state
   }
