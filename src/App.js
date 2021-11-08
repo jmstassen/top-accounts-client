3a14b0e2fdced4ea2add31fb8e84e3a24bbba05d
John M. Stassen <john.stassen@gmail.com>
2021-11-07 21:54:48 -0800

    updating top level containers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import SummaryContainer from './containers/SummaryContainer';
-import ObjectivesContainer from './containers/ObjectivesContainer';
 import AccountsContainer from './containers/AccountsContainer';
-import ActivitiesContainer from './containers/ActivitiesContainer';
+import AccountContainer from './containers/AccountContainer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="app">
-        <div className="summary-container">
-          <SummaryContainer account={this.props.current_account} />
-        </div>
-        <div className="objectives-container">
-          <ObjectivesContainer account={this.props.current_account} />
+        <div className="account-container">
+          <AccountContainer />
         </div>
         <div className="accounts-container">
           <AccountsContainer />
-        </div>
-        <div className="activity-container">
-          <ActivitiesContainer account={this.props.current_account} />
         </div>
       </div>
     );
