a8e7fdb9545613047e218ac0f88c8d3906c3e130
John M. Stassen <john.stassen@gmail.com>
2021-11-07 21:57:29 -0800

    tinker around

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -3,18 +3,19 @@ import { connect } from "react-redux";
 import AccountName from "../components/AccountName";
 
 class SummaryContainer extends Component {
-
+  
   render() {
     return (
       <div>
-        <AccountName />
+        <AccountName current_account={this.props.current_account}/>
       </div>
     ) 
   }
 }
 
 const mapStateToProps = state => ({
-   current_account: state.current_account
+   current_account: state.accountReducer.current_account
 })
+
 
 export default connect(mapStateToProps, null)(SummaryContainer)
