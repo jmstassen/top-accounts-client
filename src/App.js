b3d45715d53c32e7b0e6ead63fb8f89e5715ee63
John M. Stassen <john.stassen@gmail.com>
2021-11-08 18:11:48 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import AccountsContainer from './containers/AccountsContainer';
-import AccountContainer from './containers/AccountContainer';
-import { Routes, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        {/* <div className="account-container">
-          <AccountContainer />
-        </div>
-        <div className="accounts-container">
-          <AccountsContainer />
-        </div> */}
       </div>
     );
   }
