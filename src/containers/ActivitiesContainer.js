3c8f1c8d210e2f10b19ab16f4cd6122f9b5687ea
John M. Stassen <john.stassen@gmail.com>
2021-11-08 17:38:34 -0800

    pass props to children

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import ActivityInput from "../components/ActivityInput";
+import Activities from "../components/Activities"
 
 class ActivitiesContainer extends Component {
 
   render() {
     return <div>
       <p>Activities</p>
+      <ActivityInput />
+      <Activities activities={this.props.account.activities}/>
     </div>
   }
 
 }
 
-const mapStateToProps = state => ({
-   current_account: state.current_account
-})
+// const mapStateToProps = state => ({
+//    current_account: state.current_account
+// })
 
-export default connect(mapStateToProps, null)(ActivitiesContainer)
+export default ActivitiesContainer
