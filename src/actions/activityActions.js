6d058a991bb2148f34bf10aaa298f970fe27ade6
John M. Stassen <john.stassen@gmail.com>
2021-11-08 17:35:14 -0800

    updating addActivity fetch request

--- src/actions/activityActions.js
+++ src/actions/activityActions.js
@@ -1,18 +1,18 @@
-import { ADD_NEW_ACTIVITY, EDIT_ACTIVITY, DELETE_ACTIVITY } from "./types";
+import { ADD_ACTIVITY, EDIT_ACTIVITY, DELETE_ACTIVITY } from "./types";
 
-export const createAccount = (accountData) => dispatch => {
-  console.log(accountData)
-  fetch("http://localhost:3000/accounts", {
+export const addActivity = (data) => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/activities", {
     method: 'POST',
     headers: {
       'content-type': 'application/json'
     },
-    body: JSON.stringify(accountData)
-  })
+    body: JSON.stringify(data)
+    })
     .then(res => res.json())
     .then(account => dispatch({
-      type: NEW_ACCOUNT,
+      type: ADD_ACTIVITY,
       payload: account
-    })
-  );
-};
+    })) 
+  }
+}
