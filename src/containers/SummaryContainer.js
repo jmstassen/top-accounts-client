da4208ff6d2ee37bd2a97a5c0dce77510ad64d08
John M. Stassen <john.stassen@gmail.com>
2021-11-08 18:15:18 -0800

    cleanup

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import AccountName from "../components/AccountName";
 
 function SummaryContainer(props) {
