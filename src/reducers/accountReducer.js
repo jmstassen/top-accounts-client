cbdd1f767c8e3e80bd047c53184a35c422293a84
John M. Stassen <john.stassen@gmail.com>
2021-11-09 20:27:09 -0800

    add cases for ADD_ACTIVIT Y and DELETE_ACTIVITY

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,11 +1,29 @@
-import { FETCH_ACCOUNTS, ADD_ACCOUNT } from '../actions/types';
+import { FETCH_ACCOUNTS, ADD_ACCOUNT , ADD_ACTIVITY, DELETE_ACTIVITY } from '../actions/types';
 
-export default function accountReducer(state = {accounts: [], current_account:{}}, action) {
+export default function accountReducer(state = {accounts: []}, action) {
   switch(action.type) {
     case FETCH_ACCOUNTS:
-      return {accounts: action.payload, current_account: action.payload[0]}
+      return {accounts: action.payload}
     case ADD_ACCOUNT:
       return {...state, accounts: [...state.accounts, action.payload]}
+    case ADD_ACTIVITY:
+      let accounts = state.accounts.map(account => {
+        if (account.id === action.payload.id) {
+          return action.payload
+        } else {
+          return account
+        }
+      })
+      return {...state, accounts: accounts}
+    case DELETE_ACTIVITY:
+      let accountsDel = state.accounts.map(account => {
+        if (account.id === action.payload.id) {
+          return action.payload
+        } else {
+          return account
+        }
+      })
+      return {...state, accounts: accountsDel}
     default:
       return state
   }
