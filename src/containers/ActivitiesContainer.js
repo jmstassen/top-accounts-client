57116c4f129166af1c5373e532c0bcba712d7983
John M. Stassen <john.stassen@gmail.com>
2021-11-09 20:26:20 -0800

    clean up

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -3,11 +3,11 @@ import ActivityInput from "../components/ActivityInput";
 import Activities from "../components/Activities"
 
 class ActivitiesContainer extends Component {
-
+  
   render() {
     return <div>
       <p>Activities</p>
-      <ActivityInput />
+      <ActivityInput account={this.props.account}/>
       <Activities activities={this.props.account.activities}/>
     </div>  
   }
