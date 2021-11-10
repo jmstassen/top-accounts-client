522cf87c066c86dc2c649cd6aee52e2df1337dfb
John M. Stassen <john.stassen@gmail.com>
2021-11-09 20:25:25 -0800

    connect to store, map deleteActivity to props

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteActivity } from '../actions/activityActions'
 
 const Activities = (props) => {
 
-  
+   const handleDelete = (activity) => {
+    props.deleteActivity(activity.id, activity.account_id)
+   }
 
     return (
         <div>
           {props.activities && props.activities.map(activity => 
         <div key={activity.id}>
-          <p>{activity.activity_date} - {activity.activity_type} : {activity.activity_name}</p>
+          <p>{activity.activity_date} - {activity.activity_type} : {activity.activity_name} <button onClick={() => handleDelete(activity)}>delete</button></p>
         </div>
       )}
         </div>
     )
 }
 
-export default Activities
+export default connect(null, { deleteActivity })(Activities)
