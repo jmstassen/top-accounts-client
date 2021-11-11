c45439d26eb0a63de0371af46d8697e3eff31466
John M. Stassen <john.stassen@gmail.com>
2021-11-11 14:16:35 -0800

    clean up

--- src/components/AccountName.js
+++ src/components/AccountName.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 const AccountName = (props) => {
 
-// let account = props.accounts[props.match.params.id - 1]
-
   return (
     <div>
       <h3>{props.account && props.account.account_name}</h3>
     </div>
   )
-
 }
 
 export default AccountName
