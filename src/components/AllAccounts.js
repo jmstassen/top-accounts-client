fc4b903cf7836357f27e2a0bb3583ca3528c21cc
John M. Stassen <john.stassen@gmail.com>
2021-11-11 14:19:18 -0800

    create and build out AllAccounts page component

---
+++ src/components/AllAccounts.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import AccountsList from './AccountsList';
+import AccountInput from './AccountInput';
+import { connect } from 'react-redux';
+import { fetchAccounts } from "../actions/accountActions";
+
+
+class AllAccounts extends Component {
+
+    componentDidMount() {
+        this.props.fetchAccounts()
+      }
+
+  render () {
+      return (
+    <div className="account-container">
+      <AccountsList accounts={this.props.accounts}/>
+      <br></br>
+      <AccountInput />
+    </div>
+)
+}
+}
+
+const mapStateToProps = state => {
+    return {
+      accounts: state.accountReducer.accounts
+    }
+  }
+  
+  export default connect(mapStateToProps, {fetchAccounts})(AllAccounts)
