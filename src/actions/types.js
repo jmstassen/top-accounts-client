f6ebc2208f7a00949eae663923c71d77d0f34e88
John M. Stassen <john.stassen@gmail.com>
2021-11-10 20:17:23 -0800

    export DELETE_ACCOUNT

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,7 +5,7 @@ export const ADD_ACCOUNT = 'ADD_ACCOUNT';
 
 // export const EDIT_ACCOUNT_NAME = 'EDIT_ACCOUNT_NAME';
 
-// export const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
+export const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
 
 export const ADD_ACTIVITY = 'ADD_NEW_ACTIVITY'
 // export const EDIT_ACTIVITY = 'EDIT_ACTIVITY';
