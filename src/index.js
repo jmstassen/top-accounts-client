5cabb53b790701653e28a0c95163ce46658f607d
John M. Stassen <john.stassen@gmail.com>
2021-11-10 20:19:26 -0800

    move routes out of this file

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import AccountsContainer from './containers/AccountsContainer';
-import AccountContainer from './containers/AccountContainer';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 import App from './App';
 
@@ -21,26 +19,12 @@ let store = createStore(
 ReactDOM.render(
     <Provider store={store}>
       <Router>
-        <App />
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/accounts/1">Accounts</Link>
+      <nav>
+          <Link to="/">Home</Link> | <Link to="/accounts">Accounts</Link>
         </nav>
-        
-        <Routes>
-          <Route path="/" element={ <div className="accounts-container"><AccountsContainer /></div>} />
-          <Route path="accounts" element={<div className="app">
-            <div className="account-container">
-              <AccountContainer />
-            </div>
-            <div className="accounts-container">
-              <AccountsContainer />
-            </div>
-          </div>}> 
-          <Route path='/accounts/:id' element={ () => <div className="account-container">
-            <AccountContainer />
-          </div>} />
-          </Route>
-        </Routes>
+        <div className="app">
+          <App />
+          </div>
       </Router>
     </Provider>,
   document.getElementById('root')
