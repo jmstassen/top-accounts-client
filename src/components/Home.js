52e0d74045b313db4371c723c8962ce79d64be14
John M. Stassen <john.stassen@gmail.com>
2021-11-12 14:08:28 -0800

    edit copy

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ return (
     <h2>Top Accounts</h2>
     <br></br>
     <ul>
-        <li>Visualize and coordinate engagement with external contacts</li>
+        <li>Track and coordinate engagement with external contacts</li>
     <li>Plan future activities and keep a record of past activities</li>
     <li>Add emails, meetings, events, financial activities, documents, account summaries, or objectives</li>
     </ul>
