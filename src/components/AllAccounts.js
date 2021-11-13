3b386ea5cf1592a1e15bbca5b91af8fea9858215
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:08:11 -0800

    clean up file

--- src/components/AllAccounts.js
+++ src/components/AllAccounts.js
@@ -7,25 +7,25 @@ import { fetchAccounts } from "../actions/accountActions";
 
 class AllAccounts extends Component {
 
-    componentDidMount() {
-        this.props.fetchAccounts()
-      }
+  componentDidMount() {
+    this.props.fetchAccounts()
+  }
 
   render () {
-      return (
-    <div className="accounts-container">
-      <AccountsList accounts={this.props.accounts}/>
-      <br></br>
-      <AccountInput />
-    </div>
-)
-}
+    return (
+      <div className="accounts-container">
+        <AccountsList accounts={this.props.accounts}/>
+        <br></br>
+        <AccountInput />
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-      accounts: state.accountReducer.accounts
-    }
+  return {
+    accounts: state.accountReducer.accounts
   }
+}
   
-  export default connect(mapStateToProps, {fetchAccounts})(AllAccounts)
+export default connect(mapStateToProps, {fetchAccounts})(AllAccounts)
