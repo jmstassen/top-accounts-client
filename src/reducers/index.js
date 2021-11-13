13b25feabc2aeace2c0bdb51ac1dde962ed500bd
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:12:57 -0800

    remove activities reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import accountReducer from './accountReducer';
-// import activities from './activities';
 
 const rootReducer = combineReducers({
   accountReducer,
-  // activities
 });
 
 export default rootReducer
