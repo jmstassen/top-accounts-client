df4e81e89766b91a82ca859bee77ff6bb29c7321
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:07:40 -0800

    clean up file

--- src/components/ActivityInput.js
+++ src/components/ActivityInput.js
@@ -27,34 +27,40 @@ class ActivityInput extends Component {
         <h3>Add new activity:</h3>
         <br></br>
         <form onSubmit={this.handleSubmit}>
-            <label>Date: </label><input
-            type="date"
-            placeholder="activity date"
-            value={this.state.activity_date}
-            name="activity_date"
-            onChange={this.handleChange}
-            /><br></br><br></br>
-          <label>Type: </label><select
-            placeholder="activity type"
-            value={this.state.activity_type}
-            name="activity_type"
-            onChange={this.handleChange}>
-                <option>email</option>
-                <option>meeting</option>
-                <option>event</option>
-                <option>document</option>
-                <option>financial</option>
-                <option>summary</option>
-                <option>objective</option>
-                </select><br></br><br></br>
-                <label>Description: </label><input
-            type="text"
-            size="50"
-            placeholder="activity description"
-            value={this.state.activity_name}
-            name="activity_name"
-            onChange={this.handleChange}
-            /><br></br><br></br>
+          <label>Date: </label>
+          <input
+          type="date"
+          placeholder="activity date"
+          value={this.state.activity_date}
+          name="activity_date"
+          onChange={this.handleChange}
+          />
+          <br></br><br></br>
+          <label>Type: </label>
+          <select
+          placeholder="activity type"
+          value={this.state.activity_type}
+          name="activity_type"
+          onChange={this.handleChange}>
+            <option>email</option>
+            <option>meeting</option>
+            <option>event</option>
+            <option>document</option>
+            <option>financial</option>
+            <option>summary</option>
+            <option>objective</option>
+          </select>
+          <br></br><br></br>
+          <label>Description: </label>
+          <input
+          type="text"
+          size="50"
+          placeholder="activity description"
+          value={this.state.activity_name}
+          name="activity_name"
+          onChange={this.handleChange}
+          />
+          <br></br><br></br>
           <input type="submit" value="add activity"/>
         </form>
       </div>
