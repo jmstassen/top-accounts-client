186ecd128c51006d33d69dbf615821a7b7b4d808
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:08:35 -0800

    clean up file

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,19 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
 
-return (
+  return (
     <div className="home-container">
-    <h2>Top Accounts</h2>
-    <br></br>
-    <ul>
+      <h2>Top Accounts</h2>
+      <br></br>
+      <ul>
         <li>Track and coordinate engagement with external contacts</li>
-    <li>Plan future activities and keep a record of past activities</li>
-    <li>Add emails, meetings, events, financial activities, documents, account summaries, or objectives</li>
-    </ul>
-    <br></br>
-    <p> Click on <Link to="/accounts">Accounts</Link> to see your list of accounts and then choose one to see more detail</p>
-    
+        <li>Plan future activities and keep a record of past activities</li>
+        <li>Add emails, meetings, events, financial activities, documents, account summaries, or objectives</li>
+      </ul>
+      <br></br>
+      <p> Click on <Link to="/accounts">Accounts</Link> to see your list of accounts and then choose one to see more detail.</p>
     </div>
-)
+  )
 }
 
 export default Home
