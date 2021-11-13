fca94465620d60e5593f5b7991b9df7820eae7bc
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:06:17 -0800

    clean up file and change == to ===

--- src/components/AccountsList.js
+++ src/components/AccountsList.js
@@ -7,21 +7,22 @@ const AccountsList = (props) => {
 
   const handleDelete = (account) => {
     var result = window.confirm("Are you sure you want to delete this Account?");
-    if (result==true) {
+    if (result === true) {
       props.deleteAccount(account.id);
-   }
+    }
   }
 
   return (
     <div>
+      <table><tbody>
       {props.accounts.map(account => 
-        <div key={account.id}>
-            <p><Link to={`/accounts/${account.id}`}>{account.account_name}</Link> | <button onClick={() => handleDelete(account)}>delete</button></p>
-        </div>
-      )}
+        <tr key={account.id}>
+            <td className="account"><Link to={`/accounts/${account.id}`}>{account.account_name}</Link></td><td className="account"><button onClick={() => handleDelete(account)}>delete</button></td><td className="account"><Link to={`/accounts/${account.id}/edit`}>edit</Link></td>
+        </tr>
+      )}</tbody>
+      </table>
     </div>
   )
-  
 }
 
 export default connect(null, { deleteAccount })(AccountsList)
